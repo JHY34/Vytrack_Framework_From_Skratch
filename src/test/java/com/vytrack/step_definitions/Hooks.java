0e1f7fe7c695cad7a5a234fce687a4d7d800a2a0
JHY34 <hamityilmaz34@gmail.com>
2020-12-14 23:37:54 -0600

    @After Annotation

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -2,12 +2,18 @@ package com.vytrack.step_definitions;
 
 // this is the class to complete @BeforeAll, @AfterAll, @BeforeE
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
 
     @Before
     public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
 
     }
 
