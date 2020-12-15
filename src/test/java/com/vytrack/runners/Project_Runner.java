14c8f42322bc8298509d11c02a8547a6c734787d
JHY34 <hamityilmaz34@gmail.com>
2020-12-14 23:23:11 -0600

    runner class updated

--- src/test/java/com/vytrack/runners/Project_Runner.java
+++ src/test/java/com/vytrack/runners/Project_Runner.java
@@ -1,6 +1,12 @@
 package com.vytrack.runners;
 
+// This is the class project will be triggered
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
 public class Project_Runner {
-    // This is the class project will be triggered
+
 
 }
