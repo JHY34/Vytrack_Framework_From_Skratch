994eebebac7b3359c29a5b96373061846a7767f4
JHY34 <hamityilmaz34@gmail.com>
2020-12-14 23:27:29 -0600

    updates on runner

--- src/test/java/com/vytrack/runners/Project_Runner.java
+++ src/test/java/com/vytrack/runners/Project_Runner.java
@@ -6,6 +6,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {} // reports & re-run
+
+
+
+)
+
 public class Project_Runner {
 
 
