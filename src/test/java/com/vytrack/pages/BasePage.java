aeabc8869929e11cb2bf1d3835bf2a2271fc0625
JHY34 <hamityilmaz34@gmail.com>
2021-03-04 20:32:03 -0600

    created BasePage class

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -1,5 +1,13 @@
 package com.vytrack.pages;
 
-public class BasePage {
+import com.vytrack.utils.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
     // This class is going to have ALL common methods and locators
+
+    public BasePage () {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
 }
